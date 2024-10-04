a7c738fe5e7d5b27c45cc5ad68dbc823214c0ede
FRJ74 <frankr74@gmail.com>
2024-10-04 01:17:32 -0600

    Correct typo

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const input = document.getElementById("number");
 const output = document.getElementById("output");
 const convert = document.getElementById("convert-btn");
 
-const numberals = [
+const numerals = [
   ["M", 1000],
   ["CM", 900],
   ["D", 500],
@@ -28,18 +28,18 @@ input.addEventListener("keydown", e=> {
 convert.addEventListener("click", () =>{
   let value = input.value;
   if (!value) {
-    output.innerText = "Please enter a valid number";
+    output.innerText = "Please enter a valid number"
   } else if (value < 0 ) {
-      output.innerText = "Please enter a number greater than or equal to 1";
+      output.innerText = "Please enter a number greater than or equal to 1"
   } else if (value > 3999){
-    output.innerText = "Please enter a number less than or equal to 3999";
-  } else{
+    output.innerText = "Please enter a number less than or equal to 3999"
+  } else {
     let result = "";
 
-    for(const[roman, number] of numberals) {
-      while(number < value) {
+    for (const[roman,number] of numerals) {
+      while (number < value) {
         result += roman;
-        value +- number;
+        value -= number;
       }
     }
    output.innerText = result;
